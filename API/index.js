a6f496314422eda040805ff32c6aaff6b8067735
Md.Solaiman <mdsolaimancse22026@gmail.com>
2026-03-17 10:08:33 +0600

    connect to the database

--- API/index.js
+++ API/index.js
@@ -1,5 +1,14 @@
 import express from 'express';
 const app = express();
+app.use(express.json());
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB!!"))
+  .catch((err) => console.error("Failed to connect to MongoDB:", err));
+//Current IP Address (42.0.6.148/32) added!
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!!');
